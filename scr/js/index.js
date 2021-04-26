015f8bab9593fc288af3991e5cc6bf93db807fa0
Sregey Slezhov <pishuhott@gmail.com>
2021-04-26 19:59:02 +0300

    corrected errors

---
+++ scr/js/index.js
@@ -0,0 +1,6 @@
+let btnStatr = document.querySelector('.start');
+let btnHowToPlay = document.querySelector('.howToPlay')
+
+btnStatr.addEventListener('click', () => {
+    document.location.href = './scr/play.html';
+});
